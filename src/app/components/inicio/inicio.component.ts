bef9c5791e317492a7a60271003320d7e9c5872b
Javier Leonel Sosa <leo.vm.cba@gmail.com>
2023-06-27 10:50:39 -0300

    make the height, weight and age data entries dynamic

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent {
+  weight = 60;
+  age = 25;
+  height = 170;
+  gender = 'male';
 
+  changeHeigth(event: any){
+    this.height = event.target.value;    
+  }
+
+  selectedGender(gender: string){
+    this.gender = gender;
+  }
 }
